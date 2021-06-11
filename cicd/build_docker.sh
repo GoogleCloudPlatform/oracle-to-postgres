5d5e84537d33ea550a0fdbf1b8cb0c308349d46f
dhercher <dhercher@google.com>
2021-06-11 23:00:32 +0000

    build gcloud into image;

--- cicd/build_docker.sh
+++ cicd/build_docker.sh
@@ -3,9 +3,7 @@
 export PROJECT_ID=dms-heterogeneous
 
 cp -r ../../oracle .
-cp -r ~/.config/gcloud .
 gcloud builds submit --project=${PROJECT_ID} --tag=gcr.io/${PROJECT_ID}/build-oracle-to-postgres
 
 # Cleanup
 rm -rf oracle/
-rm -rf gcloud
